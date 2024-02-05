bc679f9343b4ad043a64e8a744b1b8f78d229539
pangz <pangz.lab@gmail.com>
2024-02-06 00:31:45 +0900

    Update app-verifier-manager.js
    - updated name to secondaryName instead of name
    - added scrolbar to version dropdown
    - reverse version selection

--- assets/js/app-verifier-manager.js
+++ assets/js/app-verifier-manager.js
@@ -33,9 +33,10 @@ function AppVerifierManager() {
             var items = "";
             var index = 0;
             var label = '';
+            
             releaseData.forEach(e => {
-                label = e.name;
-                if(index == releaseData.length -1) {
+                label = e.secondaryName;
+                if(index == 0) {
                     label += " - latest"
                 }
                 items += `<li><a  onclick="updateAppChecksumQR(`+index+`)" class="dropdown-item"> 📌 ` + label + `</a></li>`
@@ -46,8 +47,9 @@ function AppVerifierManager() {
         showReleaseQrDetails: (qrContainer, versionContainer, versionChecksumContainer, data) => {
             const qrSize = screenWidth < 450 ? screenWidth - 150 : 350;
             const version = data.name;
+            const label = data.secondaryName;
             const checksum = data.hash + '-' + data.checksum;
-            const versionLink = `<a href="`+repoBaseUrl+version+`" target='_blank'class="link-dark">${version} <i class="bi-box-arrow-in-up-right"></i></a>`;
+            const versionLink = `<a href="`+repoBaseUrl+version+`" target='_blank'class="link-dark">${label} <i class="bi-box-arrow-in-up-right"></i></a>`;
             $(versionContainer).html(versionLink);
             $(versionChecksumContainer).html(checksum);
             
